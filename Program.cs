da72029bd4159f8ee2382043e26583903085160b
Poted <35545084+Poted@users.noreply.github.com>
2022-12-11 18:20:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,35 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 class Program 
 {
     static void Main(string[] args)
     {
-        var stopwatch = new Stopwatch();
-        int[]   arr = {1, 3, 6, 4, 1, 2, 919191}, //5
-        //              1, 1, 2, 3, 4, x, 6
-                aar = {0},          //4
-                rar = {-1, -2, 0, 1, 91827364};           //1
+        int[]   arr = {1, 3, 6, 4, 1, 2, 919191},
+                aar = {0},
+                rar = {-1, -2, 0, 1, 91827364
+            };
+            
+        var method = MissingInteger(arr);
+        var method2 = MissingInteger(aar);
+        var method3 = MissingInteger(rar);
 
-            
-            
+        System.Console.WriteLine(  
+            "First method exTime: " + TimeMethod(() => MissingInteger(arr)) + "\n" +
+            "Second method exTime: " + TimeMethod(() => MissingInteger(aar)) + "\n" +
+            "Third method exTime: " + TimeMethod(() => MissingInteger(rar))
+        );
+
         System.Console.WriteLine(
-            TimeMethod(() => Method1(1)) + 
-            " arr: " + TimeMethod(() => MissingInteger(arr)) +
+            " arr: " + MissingInteger(arr) +
             "\n aar: " + MissingInteger(aar) +
             "\n rar: " + MissingInteger(rar)
-            );
-
-        
-
+        );
     }
-
-    static void Method1(int a)
-    {
-        int i = 0;
-        int i2 = i + 1;
-
-    }
-
 
     static long TimeMethod(Action methodToTime)
-    {
+    {//Counts time of method passed as a parameter to execute in miliseconds;
+        //use with lambda if it has parameters: TimeMethod(() => SomeMethod())
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         methodToTime();
@@ -43,7 +39,8 @@ class Program
 
 
     private static int MissingInteger(int[] arr)
-    {
+    {//returns the lowest integer that is not in the array;
+
         HashSet<uint> hs = new HashSet<uint>();
         var orderedArray = arr.OrderBy(x => x);
         int ret = new int();
@@ -71,7 +68,8 @@ class Program
     }
 
     private static bool PermCheck(int[] arr)
-    {
+    {//Return true if array has only one occurance of int one after each other
+
         // //Sample input:
         // int[]   arr = {1, 2, 4, 5, 89, 144, 3}, //false
         //         aar = {1, 2, 5, 4, 3},          //true
@@ -95,17 +93,17 @@ class Program
             if(hs.Count() != arr.Max())
             return false;
         }
-
-
+        
         return true;
     }
 
-    public static string ReturnMultipliedChars(string S) {
+    public static string ReturnMultipliedChars(string S) 
+    {//Returns a chars that are multiplied in a string;
 
         string ret = string.Empty;
 
         var cj =
-        S.ToLower().GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
+        S.ToLower().GroupBy(x => x).Where(x => x.Count() > 1).Select(x => x.Key).ToList();
        
        ret = string.Join(",", cj.ToArray());
        return ret;
